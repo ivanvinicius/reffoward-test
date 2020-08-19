b5257b4f36e054d9e223b41e13136a4cbf77c488
ivanvinicius <ivanvinicius88@gmail.com>
2020-08-19 18:41:50 -0300

    first commit

---
+++ src/components/Select/index.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Select: React.FC = () => {
+  return <h1>Select Component</h1>;
+}
+
+export default Select;
---
+++ src/pages/Form/index.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import Select from '../../components/Select';
+
+const Form: React.FC = () => {
+  return (
+    <>
+    <h1>Form Component</h1>
+
+    <Select />
+    </>
+  );
+}
+
+export default Form;
